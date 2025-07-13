ca85698152b1090fa1df2b8f2d5c5f4029e2a5ae
Rashard Iman Kelly @nasa-jpl @stateofcalifornia <rashardiman@outlook.com>
2025-07-13 16:06:52 -0700

    Palo Alto 155.190.2.33=stale
    Signed-off-by: Rashard Iman Kelly @nasa-jpl @stateofcalifornia <rashardiman@outlook.com>

---
+++ assets/plots/lbc.sh
@@ -0,0 +1,36 @@
+#!/data/data/com.termux/files/usr/bin/bash
+date
+uptime 
+uname -a
+cal &&  date
+echo "can we talkto @nasa?"
+ifconfig -a
+traceroute lacity.gov
+traceroute lacity.org
+uptime
+curl http://nasa.gov
+traceroute thehollywoodtheater.com
+traceroute hofoco.org
+curl http://thehollywoodtheater.com
+curl http://whitehouse.gov
+traceroute google.com
+nslookup jpl.nasa.gov
+dig -x whitehouse.gov
+dig -x lacity.gov
+dig -x jpl.nasa.gov
+dig -x metro.net
+traceroute metro.net
+traceroute nasa.gov
+traceroute whitehouse.gov
+traceroute netflix.com
+traceroute hulu.com
+traceroute cnn.com
+traceroute pornhub.com
+traceroute youtube.com
+traceroute wikipedia.org
+traceroute cia.gov
+traceroute playboy.com
+traceroute ice.gov 
+ping -c 2 ice.gov
+traceroute 216.81.59.173
+cp *.* storage/shared/Documents/
