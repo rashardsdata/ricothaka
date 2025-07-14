385f4eaeb2f2e8023cbe832adff823e799c5853b
Rashard Iman Kelly @nasa-jpl @stateofcalifornia <rashardiman@outlook.com>
2025-07-14 11:57:39 -0700

    Update lbc.sh
    Signed-off-by: Rashard Iman Kelly @nasa-jpl @stateofcalifornia <rashardiman@outlook.com>

--- assets/plots/lbc.sh
+++ assets/plots/lbc.sh
@@ -3,36 +3,41 @@ date
 uptime 
 cal &&  date
 echo "can we talkto @nasa?"
-echo "can we talkto @nasa?"
-netstat -tp
-netstat -s
-South Pasadena 205.155.237.66
-active
-Your current session
-Seen in US
-Los Angeles 64.183.42.232 
+echo "Discovered in South PAsaDena "
+Traceroute "205.155.237.66"
+echo "GithubSession"
+echo "Seen in USLos Angeles"
+Trahceroute "64.183.42.232" 
 ifconfig -a
 traceroute lacity.gov
 traceroute lacity.org
 uptime
 curl http://nasa.gov
-traceroute thehollywoodtheater.com
+dig -x hofoco.org
+curl http://hofoco.org
 traceroute hofoco.org
 curl http://thehollywoodtheater.com
+traceroute thehollywoodtheater.com
 host google.com
 traceroute google.com
 nslookup jpl.nasa.gov
 whois nasa.gov
 curl http://whitehouse.gov
 ping -c 2 whitehouse.gov
+traceroute whitehouse.gov
 dig -x whitehouse.gov
 dig -x lacity.gov
 dig -x jpl.nasa.gov
 dig -x metro.net
 traceroute metro.net
+echo "can we talkto @nasa?"
+ping -c 2 nasa.gov
+ping -c 2 jpl.nasa.gov
+nslookup noaa.gov
+dig noaa.gov ANY
+ping -c 2 noaa.gov
 traceroute nasa.gov/msd
 traceroute "155.190.2.33"
-traceroute whitehouse.gov
 traceroute netflix.com
 traceroute "205.155.237.66"
 traceroute hulu.com
@@ -45,4 +50,6 @@ traceroute playboy.com
 traceroute ice.gov 
 ping -c 2 ice.gov
 traceroute "216.81.59.173"
+dig -x ice.gov 
+
 cp *.* storage/shared/Documents/
