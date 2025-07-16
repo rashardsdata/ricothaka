215cdb94c79da100648f1d049a5f4bc947bd833d
[rashardci](rashard_iman_kelly @nasa-jpl) <rashardci@protonmail.com>
2025-07-15 19:17:36 -0700

    Update traceroutedemo.sh

--- assets/plots/traceroutedemo.sh
+++ assets/plots/traceroutedemo.sh
@@ -11,3 +11,21 @@ traceroute 127.0.0.1
 sleep .5 # Waits 0.5 second.
 ping -c 2 localhost
 echo "Nigh der ya go!"
+
+South Pasadena 205.155.237.66
+active
+Your current session
+Seen in US
+Los Angeles 155.190.3.5
+active
+Last accessed on Jul 14, 2025
+Seen in US
+Norwalk 107.77.227.55
+stale
+Last accessed on Jul 13, 2025
+Seen in US
+South Pasadena 205.155.237.66
+stale
+Last accessed on Jul 13, 2025
+Seen in US 
+ 
